c6170f79f754cc64af70e5093948b0639fb73f26
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-07-13 11:39:44 -0500

    write test solutions

--- lib/dog.py
+++ lib/dog.py
@@ -8,8 +8,32 @@ APPROVED_BREEDS = [
     "Beagle",
     "French Bulldog",
     "Pug",
-    "Pointer"
+    "Pointer",
 ]
 
+
 class Dog:
-    pass
+    def __init__(self, name="fido", breed="Mastiff"):
+        self.name = name
+        self.breed = breed
+
+    def set_name(self, name):
+        if type(name) == str and len(name) >= 1 and len(name) <= 25:
+            self._name = name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_name(self):
+        return self._name
+
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    def get_breed(self):
+        return self._breed
+
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
